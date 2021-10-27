fef960287d82eaa2898c15893b3afb00bb3c5487
KatoJun <jun.kato0208@gmail.com>
2021-10-27 14:05:35 +0900

    axiosのインストールまで

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import "./styles.css";
+import { UserCard } from "./components/UserCard";
+import { UserProfile } from "./types/userProfile";
+
+const user = {
+  id: 1,
+  name: "yamada",
+  email: "1111@example.xom",
+  address: "ADDRESS"
+};
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <UserCard user={user} />
     </div>
   );
 }
